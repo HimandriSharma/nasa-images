e03ea7dfe9d8077161374c0afd98f71fb99cff08
HimandriSharma <himandrisharma27@gmail.com>
2023-02-01 16:03:09 +0530

    Spotlight changes completed

--- src/components/Spotlight.tsx
+++ src/components/Spotlight.tsx
@@ -5,22 +5,22 @@ import { Inter } from "@next/font/google";
 import { connect } from "react-redux";
 import { useEffect, useState } from "react";
 import Router from "next/router";
+import { PlayCircleOutlined } from "@ant-design/icons";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const sampleData = {
-	copyright: "Tunc Tezel",
-	date: "2022-10-01",
-	explanation:
-		"Observe the Moon every night and you'll see its visible sunlit portion gradually change. In phases progressing from New Moon to Full Moon to New Moon again, a lunar cycle or lunation is completed in about 29.5 days. Top left to bottom right, this 7x4 matrix of telescopic images captures the range of lunar phases for 28 consecutive nights, from the evening of July 29 to the morning of August 26, following an almost complete lunation. No image was taken 24 hours or so just after and just before New Moon, when the lunar phase is at best a narrow crescent, close to the Sun and really hard to see. Finding mostly clear Mediterranean skies required an occasional road trip to complete this lunar cycle project, imaging in early evening for the first half and late evening and early morning for the second half of the lunation. Since all the images are registered at the same scale you can use this matrix to track the change in the Moon's apparent size during the single lunation. For extra credit, find the lunar phase that occurred closest to perigee.  Tonight: International Observe the Moon Night",
-	hdurl: "https://apod.nasa.gov/apod/image/2210/Lu20220729-0826.jpg",
-	media_type: "image",
-	service_version: "v1",
-	title: "Lunation Matrix",
-	url: "https://apod.nasa.gov/apod/image/1301/cygnuswall_pavelchak_1600.jpg",
-};
 function Spotlight({ spotlight, changeOverlay }: any) {
-	const [spot, setSpot] = useState(sampleData);
+	const [spot, setSpot] = useState({
+		copyright: "",
+		date: "",
+		explanation: "",
+		hdurl: "",
+		media_type: "",
+		service_version: "",
+		title: "",
+		url: "",
+		thumbnail_url: "",
+	});
 	useEffect(() => {
 		setSpot(spotlight.spotlight);
 	}, [spotlight.spotlight]);
@@ -40,19 +40,53 @@ function Spotlight({ spotlight, changeOverlay }: any) {
 					<br />
 					<p className={inter.className}>{spot.explanation}</p>
 					<br />
-					<p className={inter.className}>
-						authored by <b>{spot.copyright}</b>
-					</p>
+					{spot.copyright ? (
+						<p className={inter.className}>
+							authored by <b>{spot.copyright}</b>
+						</p>
+					) : (
+						<></>
+					)}
 				</Col>
 				<Col sm={16} md={12} lg={10}>
-					<Image
-						loader={() => spot.url}
-						src={spot.url}
-						alt="NASA Logo"
-						width={500}
-						height={350}
-						style={{ borderRadius: "1rem" }}
-					/>
+					{spot.media_type === "image" ? (
+						<Image
+							loader={() => spot.url}
+							src={spot.url}
+							alt="NASA Logo"
+							width={500}
+							height={350}
+							style={{ borderRadius: "1rem" }}
+						/>
+					) : spot.media_type === "video" ? (
+						<div
+							style={{
+								borderRadius: "0.5rem",
+								height: "200px",
+								backgroundPosition: "center",
+								backgroundSize: "cover",
+								display: "grid",
+								placeItems: "center",
+								backgroundRepeat: "no-repeat",
+								backgroundImage: `url(${spot.thumbnail_url})`,
+							}}
+						>
+							<div
+								style={{
+									display: "flex",
+									flexDirection: "row",
+									alignItems: "center",
+									justifyContent: "center",
+									color: "white",
+									width: "100%",
+								}}
+							>
+								<PlayCircleOutlined style={{ fontSize: "350%" }} />
+							</div>
+						</div>
+					) : (
+						<h3>Error in Loading Media Type</h3>
+					)}
 				</Col>
 			</Row>
 		</div>
